a3f573b9006e3bace6430aa7c347a624a5b94a75
noulife <escudero.elton@gmail.com>
2025-02-26 15:36:37 +0800

    feat: Added Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Countdown from "./components/Countdown";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 
 const App = () => {
   return (
-    <>
-      <Countdown />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Countdown />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
